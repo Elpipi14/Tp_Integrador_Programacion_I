89734dc3663b23a4c436045c570c5fe510aa33a9
Elpipi14 <andsrissp1@gmail.com>
2025-06-09 11:07:21 -0300

    TP Integral Programacion I

---
+++ main.py
@@ -0,0 +1,31 @@
+from core.logging_config import logger
+from preprocessing.cargar_y_preprocesar import preparar_datos
+from modelo_arbol.arbol_decision import construir_arbol, predecir, imprimir_arbol
+from modelo_arbol.metricas import calcular_metricas
+import graficos
+
+logging = logger
+
+# Datos
+X, y, df_balanceado = preparar_datos('data/data.csv')
+
+# Entrenamiento
+logging.info("Entrenando árbol de decisión...")
+arbol = construir_arbol(X, y, profundidad_max=6)
+
+# Predicción
+logging.info("Evaluando árbol...")
+y_pred = X.apply(lambda fila: predecir(arbol, fila), axis=1)
+
+# Métricas
+calcular_metricas(y, y_pred)
+
+# Mostrar árbol
+logging.info("Estructura del árbol:")
+imprimir_arbol(arbol)
+
+# Guardar resultados
+df_balanceado['prediccion_pico'] = y_pred
+
+# Gráficos
+graficos.grafico_comparacion_pico_real_vs_predicho(df_balanceado)
